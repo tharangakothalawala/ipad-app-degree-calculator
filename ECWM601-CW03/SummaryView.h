9b9c6fbf2650d10714fa8702316b4ecfa448a24a
Tharanga Kothalawala <tharanga.kothalawala@gmail.com>
2013-05-16 12:54:14 +0100

    added the summaryView graph

---
+++ ECWM601-CW03/SummaryView.h
@@ -0,0 +1,13 @@
+//
+//  SummaryView.h
+//  ECWM601-CW03
+//
+//  Created by Snow Leopard User on 16/05/2013.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SummaryView : UIView
+
+@end
